e034ff92fedf27786ea0f208b2bccc2b4fe306fb
wongsaphat puangsorn <u2gstudio@gmail.com>
2024-02-13 10:02:48 +0700

    add photo

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,43 @@
+<template>
+  <AppHeroSection />
+  <div class="mx-auto items-center gap-x-8 gap-y-16 p-2 lg:px-8">
+    <div class="mx-auto max-w-4xl">
+      <h2 class="text-3xl text-gray-900 sm:text-4xl mx-auto">
+        RS TROPHY ถ้วยรางวัลคุณภาพดี
+      </h2>
+      <p class="mt-4 text-gray-500 mx-auto">
+        RS TROPHY บริษัทจัดจำหน่ายถ้วยรางวัลคุณภาพดีแข็งแรงทนทาน
+        พร้อมมีทีมกราฟิกดีไซน์บริการการออกแบบฟรี
+        โดยเรามีทั้งในรูปแบบหน้าร้านและออนไลน์พร้อมบริการจัดส่งทั่วประเทศไทย
+      </p>
+    </div>
+    <div class="py-4">
+      <h1 class="text-center">ถ้วยรางวัล</h1>
+      <div class="mx-auto">
+        <div v-for="(product, index) in products" :key="index">
+          <h2 class="text-center">ถ้วยรางวัลรหัส {{ product.name }}</h2>
+          <div
+            class="gap-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+          >
+            <img
+              v-for="index in product.photo"
+              :key="index"
+              :src="`/catalog/${product.name}/${index}.jpg`"
+              width="295px"
+              height="295px"
+              loading="lazy"
+              alt="ผลงานถ้วยรางวัลที่ผ่านมา"
+              title="ผลงานถ้วยรางวัลที่ผ่านมา"
+              aria-label="ผลงานถ้วยรางวัลที่ผ่านมา"
+              class="scaleup"
+            />
+          </div>
+        </div>
+      </div>
+      <AppOurCustomer />
+    </div>
+  </div>
+</template>
 <script setup lang="ts">
 const title = "RS TROPHY | ถ้วยรางวัล คุณภาพดีแข็งแรงทนทาน";
 const description =
@@ -12,33 +52,48 @@ useSeoMeta({
   twitterDescription: description,
   ogImage: image,
   twitterImage: image,
-  keywords:
-    "RS TROPHY, ถ้วยรางวัล",
+  keywords: "RS TROPHY, ถ้วยรางวัล",
 });
+const products = [
+  {
+    photo: 29,
+    name: "B",
+  },
+  {
+    photo: 3,
+    name: "T",
+  },
+  {
+    photo: 2,
+    name: "FOOTBALL",
+  },
+  {
+    photo: 12,
+    name: "D",
+  },
+  {
+    photo: 3,
+    name: "L",
+  },
+  {
+    photo: 2,
+    name: "LP",
+  },
+  {
+    photo: 36,
+    name: "IT-1",
+  },
+  {
+    photo: 12,
+    name: "IT-2",
+  },
+  {
+    photo: 6,
+    name: "IT-4",
+  },
+  {
+    photo: 6,
+    name: "IT-5",
+  },
+];
 </script>
-<template>
-  <AppHeroSection />
-  <div class="mx-auto items-center gap-x-8 gap-y-16 p-2 lg:px-8">
-    <div class="mx-auto max-w-4xl">
-      <h2 class="text-3xl text-gray-900 sm:text-4xl mx-auto">
-        RS TROPHY ถ้วยรางวัลคุณภาพดี
-      </h2>
-      <p class="mt-4 text-gray-500 mx-auto">
-        RS TROPHY บริษัทจัดจำหน่ายถ้วยรางวัลคุณภาพดีแข็งแรงทนทาน พร้อมมีทีมกราฟิกดีไซน์บริการการออกแบบฟรี
-        โดยเรามีทั้งในรูปแบบหน้าร้านและออนไลน์พร้อมบริการจัดส่งทั่วประเทศไทย
-      </p>
-    </div>
-    <div class="py-4">
-      <h1 class="text-center">ถ้วยรางวัล</h1>
-      <div class="mx-auto">
-        <h2 class="text-center">ผลงานที่ผ่านมา</h2>
-        <div class="gap-2 grid grid-cols-2 md:grid-cols-4">
-          <img v-for="index in [3, 4, 5, 6]" :key="index" :src="`/custom_acrylic/${index}.webp`" width="295px"
-            height="295px" loading="lazy" alt="ผลงานถ้วยรางวัลที่ผ่านมา" title="ผลงานถ้วยรางวัลที่ผ่านมา"
-            aria-label="ผลงานถ้วยรางวัลที่ผ่านมา" class="scaleup" />
-        </div>
-      </div>
-      <AppOurCustomer />
-    </div>
-  </div>
-</template>
