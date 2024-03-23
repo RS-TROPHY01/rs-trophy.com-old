a6632fb5ecd4f1ea59a3b8b801e01f2f2bc79694
NooobtimeX <u2gstudio@gmail.com>
2024-03-23 09:43:15 +0700

    fix url

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -36,8 +36,8 @@ export default {
     return {
       links: [
         { text: "หน้าแรก", href: "/" },
-        { text: "ถ้วยรางวัล", href: "https://www.rs-trophy.com/" },
-        { text: "เกี่ยวกับเรา", href: "https://www.rs-trophy.com/aboutus" },
+        { text: "ถ้วยรางวัล", href: "/product" },
+        { text: "เกี่ยวกับเรา", href: "/aboutus" },
         { text: "บทความ", href: "/blog" },
       ],
     };
